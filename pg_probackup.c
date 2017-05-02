3c707a0a839fd9e8f8d26bc37d9995d140901449
Anastasia <a.lubennikova@postgrespro.ru>
2017-05-02 11:50:20 +0300

    Version 1.1.9

--- pg_probackup.c
+++ pg_probackup.c
@@ -16,7 +16,7 @@
 #include <time.h>
 #include <sys/stat.h>
 
-const char *PROGRAM_VERSION	= "1.1.8";
+const char *PROGRAM_VERSION	= "1.1.9";
 const char *PROGRAM_URL		= "https://github.com/postgrespro/pg_probackup";
 const char *PROGRAM_EMAIL	= "https://github.com/postgrespro/pg_probackup/issues";
 
