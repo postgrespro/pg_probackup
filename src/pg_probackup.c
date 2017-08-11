9b784015db3d7676842c7e88fd9d5b3ee53789a6
Arthur Zakirov <z-arthur@yandex.ru>
2017-08-11 19:05:36 +0300

    Version 2.0.2

--- src/pg_probackup.c
+++ src/pg_probackup.c
@@ -17,7 +17,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-const char *PROGRAM_VERSION	= "2.0.1";
+const char *PROGRAM_VERSION	= "2.0.2";
 const char *PROGRAM_URL		= "https://github.com/postgrespro/pg_probackup";
 const char *PROGRAM_EMAIL	= "https://github.com/postgrespro/pg_probackup/issues";
 
