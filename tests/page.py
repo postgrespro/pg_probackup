3d0fcdf9b04774b37d54049ae4987ffceefe4cf2
Grigory Smolkin <g.smolkin@postgrespro.ru>
2018-01-16 18:46:55 +0300

    tests: added test_page_vacuum_truncate

--- tests/page.py
+++ tests/page.py
@@ -2,7 +2,8 @@ import os
 import unittest
 from .helpers.ptrack_helpers import ProbackupTest, ProbackupException
 from datetime import datetime, timedelta
-import subprocess, time
+import subprocess
+import time
 
 
 module_name = 'page'
@@ -10,34 +11,92 @@ module_name = 'page'
 
 class PageBackupTest(ProbackupTest, unittest.TestCase):
 
-#    # @unittest.skip("skip")
-#    # @unittest.expectedFailure
-#    def test_page_check_archive_enabled(self):
-#        """make node, take page backup without enabled archive, should result in error"""
-#        self.maxDiff = None
-#        fname = self.id().split('.')[3]
-#        backup_dir = os.path.join(self.tmp_path, module_name, fname, 'backup')
-#        node = self.make_simple_node(base_dir="{0}/{1}/node".format(module_name, fname),
-#            set_replication=True,
-#            initdb_params=['--data-checksums'],
-#            pg_options={'wal_level': 'replica', 'max_wal_senders': '2', 'checkpoint_timeout': '30s', 'ptrack_enable': 'on'}
-#            )
-#
-#        self.init_pb(backup_dir)
-#        self.add_instance(backup_dir, 'node', node)
-#        node.start()
-#
-#        try:
-#            self.backup_node(backup_dir, 'node', node, backup_type='page', options=['--stream'])
-#            # we should die here because exception is what we expect to happen
-#            self.assertEqual(1, 0, "Expecting Error because archive_mode disabled.\n Output: {0} \n CMD: {1}".format(
-#                repr(self.output), self.cmd))
-#        except ProbackupException as e:
-#            self.assertIn('ERROR: Archiving must be enabled for PAGE backup\n', e.message,
-#                '\n Unexpected Error Message: {0}\n CMD: {1}'.format(repr(e.message), self.cmd))
-#
-#        # Clean after yourself
-#        self.del_test_dir(module_name, fname)
+    # @unittest.skip("skip")
+    def test_page_vacuum_truncate(self):
+        """make node, create table, take full backup,
+           delete last 3 pages, vacuum relation,
+           take page backup, take second page backup,
+           restore last page backup and check data correctness"""
+        fname = self.id().split('.')[3]
+        backup_dir = os.path.join(self.tmp_path, module_name, fname, 'backup')
+        node = self.make_simple_node(
+            base_dir="{0}/{1}/node".format(module_name, fname),
+            set_replication=True,
+            initdb_params=['--data-checksums'],
+            pg_options={
+                'wal_level': 'replica',
+                'max_wal_senders': '2',
+                'checkpoint_timeout': '300s'
+            }
+        )
+        node_restored = self.make_simple_node(
+            base_dir="{0}/{1}/node_restored".format(module_name, fname),
+        )
+
+        self.init_pb(backup_dir)
+        self.add_instance(backup_dir, 'node', node)
+        self.set_archiving(backup_dir, 'node', node)
+        node_restored.cleanup()
+        node.start()
+        self.create_tblspace_in_node(node, 'somedata')
+
+        node.safe_psql(
+            "postgres",
+            "create sequence t_seq; "
+            "create table t_heap tablespace somedata as select i as id, "
+            "md5(i::text) as text, "
+            "md5(repeat(i::text,10))::tsvector as tsvector "
+            "from generate_series(0,1024) i;"
+        )
+        node.safe_psql(
+            "postgres",
+            "vacuum t_heap"
+        )
+
+        self.backup_node(backup_dir, 'node', node)
+
+        node.safe_psql(
+            "postgres",
+            "delete from t_heap where ctid >= '(11,0)'"
+        )
+        node.safe_psql(
+            "postgres",
+            "vacuum t_heap"
+        )
+
+        self.backup_node(
+            backup_dir, 'node', node, backup_type='page',
+            options=['--log-level-file=verbose']
+        )
+
+        self.backup_node(
+            backup_dir, 'node', node, backup_type='page',
+            options=['--log-level-file=verbose']
+        )
+
+        if self.paranoia:
+            pgdata = self.pgdata_content(node.data_dir)
+
+        old_tablespace = self.get_tblspace_path(node, 'somedata')
+        new_tablespace = self.get_tblspace_path(node_restored, 'somedata_new')
+
+        self.restore_node(
+            backup_dir, 'node', node_restored,
+            options=["-j", "4", "-T", "{0}={1}".format(
+                old_tablespace, new_tablespace)]
+        )
+
+        # Physical comparison
+        if self.paranoia:
+            pgdata_restored = self.pgdata_content(node_restored.data_dir)
+            self.compare_pgdata(pgdata, pgdata_restored)
+
+        node_restored.append_conf(
+            "postgresql.auto.conf", "port = {0}".format(node_restored.port))
+        node_restored.start()
+
+        # Clean after yourself
+        # self.del_test_dir(module_name, fname)
 
     # @unittest.skip("skip")
     def test_page_stream(self):
@@ -45,10 +104,15 @@ class PageBackupTest(ProbackupTest, unittest.TestCase):
         self.maxDiff = None
         fname = self.id().split('.')[3]
         backup_dir = os.path.join(self.tmp_path, module_name, fname, 'backup')
-        node = self.make_simple_node(base_dir="{0}/{1}/node".format(module_name, fname),
+        node = self.make_simple_node(
+            base_dir="{0}/{1}/node".format(module_name, fname),
             set_replication=True,
             initdb_params=['--data-checksums'],
-            pg_options={'wal_level': 'replica', 'max_wal_senders': '2', 'checkpoint_timeout': '30s', 'ptrack_enable': 'on'}
+            pg_options={
+                'wal_level': 'replica',
+                'max_wal_senders': '2',
+                'checkpoint_timeout': '30s',
+                'ptrack_enable': 'on'}
             )
 
         self.init_pb(backup_dir)
@@ -59,33 +123,50 @@ class PageBackupTest(ProbackupTest, unittest.TestCase):
         # FULL BACKUP
         node.safe_psql(
             "postgres",
-            "create table t_heap as select i as id, md5(i::text) as text, md5(i::text)::tsvector as tsvector from generate_series(0,100) i")
-        full_result = node.execute("postgres", "SELECT * FROM t_heap")
-        full_backup_id = self.backup_node(backup_dir, 'node', node, backup_type='full', options=['--stream'])
+            "create table t_heap as select i as id, md5(i::text) as text, "
+            "md5(i::text)::tsvector as tsvector "
+            "from generate_series(0,100) i")
 
-        #PAGE BACKUP
+        full_result = node.execute("postgres", "SELECT * FROM t_heap")
+        full_backup_id = self.backup_node(
+            backup_dir, 'node', node,
+            backup_type='full', options=['--stream'])
+
+        # PAGE BACKUP
         node.safe_psql(
             "postgres",
-            "insert into t_heap select i as id, md5(i::text) as text, md5(i::text)::tsvector as tsvector from generate_series(100,200) i")
+            "insert into t_heap select i as id, md5(i::text) as text, "
+            "md5(i::text)::tsvector as tsvector "
+            "from generate_series(100,200) i")
         page_result = node.execute("postgres", "SELECT * FROM t_heap")
-        page_backup_id = self.backup_node(backup_dir, 'node', node, backup_type='page', options=['--stream'])
+        page_backup_id = self.backup_node(
+            backup_dir, 'node', node,
+            backup_type='page', options=['--stream'])
 
         # Drop Node
         node.cleanup()
 
         # Check full backup
-        self.assertIn("INFO: Restore of backup {0} completed.".format(full_backup_id),
-            self.restore_node(backup_dir, 'node', node, backup_id=full_backup_id, options=["-j", "4"]),
-            '\n Unexpected Error Message: {0}\n CMD: {1}'.format(repr(self.output), self.cmd))
+        self.assertIn(
+            "INFO: Restore of backup {0} completed.".format(full_backup_id),
+            self.restore_node(
+                backup_dir, 'node', node,
+                backup_id=full_backup_id, options=["-j", "4"]),
+            '\n Unexpected Error Message: {0}\n'
+            ' CMD: {1}'.format(repr(self.output), self.cmd))
         node.start()
         full_result_new = node.execute("postgres", "SELECT * FROM t_heap")
         self.assertEqual(full_result, full_result_new)
         node.cleanup()
 
         # Check page backup
-        self.assertIn("INFO: Restore of backup {0} completed.".format(page_backup_id),
-            self.restore_node(backup_dir, 'node', node, backup_id=page_backup_id, options=["-j", "4"]),
-            '\n Unexpected Error Message: {0}\n CMD: {1}'.format(repr(self.output), self.cmd))
+        self.assertIn(
+            "INFO: Restore of backup {0} completed.".format(page_backup_id),
+            self.restore_node(
+                backup_dir, 'node', node,
+                backup_id=page_backup_id, options=["-j", "4"]),
+            '\n Unexpected Error Message: {0}\n'
+            ' CMD: {1}'.format(repr(self.output), self.cmd))
         node.start()
         page_result_new = node.execute("postgres", "SELECT * FROM t_heap")
         self.assertEqual(page_result, page_result_new)
