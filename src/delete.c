901062188d44dfb44aec3632059a4ca570396574
Arthur Zakirov <z-arthur@yandex.ru>
2018-12-06 16:03:29 +0300

    Show progress messages with INFO verbose level

--- src/delete.c
+++ src/delete.c
@@ -256,6 +256,7 @@ delete_backup_files(pgBackup *backup)
 	char		path[MAXPGPATH];
 	char		timestamp[100];
 	parray	   *files;
+	size_t		num_files;
 
 	/*
 	 * If the backup was deleted already, there is nothing to do.
@@ -286,13 +287,14 @@ delete_backup_files(pgBackup *backup)
 
 	/* delete leaf node first */
 	parray_qsort(files, pgFileComparePathDesc);
-	for (i = 0; i < parray_num(files); i++)
+	num_files = parray_num(files);
+	for (i = 0; i < num_files; i++)
 	{
 		pgFile	   *file = (pgFile *) parray_get(files, i);
 
-		/* print progress */
-		elog(VERBOSE, "Delete file(%zd/%lu) \"%s\"", i + 1,
-			 (unsigned long) parray_num(files), file->path);
+		if (progress)
+			elog(INFO, "Progress: (%zd/%zd). Process file \"%s\"",
+				 i + 1, num_files, file->path);
 
 		if (remove(file->path))
 		{
