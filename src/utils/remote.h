9311590adf38b53c1063d6f87ee71d12f7b82de1
Konstantin Knizhnik <knizhnik@garret.ru>
2019-01-21 19:22:05 +0300

    More remote options to config

---
+++ src/utils/remote.h
@@ -0,0 +1,24 @@
+/*-------------------------------------------------------------------------
+ *
+ * remote.h: - prototypes of remote functions.
+ *
+ * Copyright (c) 2017-2018, Postgres Professional
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef REMOTE_H
+#define REMOTE_H
+
+typedef struct RemoteConfig
+{
+	char* proto;
+	char* host;
+	char* port;
+	char* path;
+	char *ssh_config;
+	char *ssh_options;
+	bool  enabled;
+} RemoteConfig;
+
+#endif
