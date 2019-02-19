883464fdfc4560667bdef96bfade77b0f39877e2
Grigory Smolkin <g.smolkin@postgrespro.ru>
2019-02-19 15:42:18 +0300

    PGPRO-1918: added test_locking_restore_locked_without_validation()

--- tests/locking.py
+++ tests/locking.py
@@ -353,9 +353,63 @@ class LockingTest(ProbackupTest, unittest.TestCase):
                     repr(self.output), self.cmd))
         except ProbackupException as e:
             self.assertTrue(
-                "Insert expected error message".format(
-                    backup_id) in e.message,
-                e.message,
+                "Insert expected error message" in e.message,
+                '\n Unexpected Error Message: {0}\n CMD: {1}'.format(
+                    repr(e.message), self.cmd))
+
+        # Clean after yourself
+        self.del_test_dir(module_name, fname)
+
+    def test_locking_restore_locked_without_validation(self):
+        """
+        make node, take full backup, take page backup,
+        launch validate on FULL and stop it in the middle,
+        launch restore of PAGE.
+        Expect restore to fail because validation of
+        intermediate backup is impossible
+        """
+        fname = self.id().split('.')[3]
+        node = self.make_simple_node(
+            base_dir=os.path.join(module_name, fname, 'node'),
+            initdb_params=['--data-checksums'],
+            pg_options={'wal_level': 'replica'})
+
+        backup_dir = os.path.join(self.tmp_path, module_name, fname, 'backup')
+        self.init_pb(backup_dir)
+        self.add_instance(backup_dir, 'node', node)
+        self.set_archiving(backup_dir, 'node', node)
+        node.slow_start()
+
+        # FULL
+        backup_id = self.backup_node(backup_dir, 'node', node)
+
+        # PAGE1
+        restore_id = self.backup_node(backup_dir, 'node', node, backup_type='page')
+
+        gdb = self.validate_pb(
+            backup_dir, 'node', backup_id=backup_id, gdb=True)
+
+        gdb.set_breakpoint('pgBackupValidate')
+        gdb.run_until_break()
+
+        node.cleanup()
+
+        try:
+            self.restore_node(
+                backup_dir, 'node', node, options=['--no-validate'])
+            self.assertEqual(
+                1, 0,
+                "Expecting Error because restore without whole chain validation "
+                "is prohibited unless --no-validate provided.\n "
+                "Output: {0} \n CMD: {1}".format(
+                    repr(self.output), self.cmd))
+        except ProbackupException as e:
+            self.assertTrue(
+                "Backup {0} is used without validation".format(
+                    restore_id) in e.message and
+                'is using backup {0} and still is running'.format(
+                    backup_id) in e.message and
+                'ERROR: Cannot lock backup directory' in e.message,
                 '\n Unexpected Error Message: {0}\n CMD: {1}'.format(
                     repr(e.message), self.cmd))
 
