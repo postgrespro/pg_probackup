5c4ad52e6de954000ae930713bae74d0cee7c710
Grigory Smolkin <g.smolkin@postgrespro.ru>
2019-10-09 21:10:02 +0300

    Version 2.2.0

--- src/pg_probackup.h
+++ src/pg_probackup.h
@@ -205,8 +205,8 @@ typedef enum ShowFormat
 #define BYTES_INVALID		(-1) /* file didn`t changed since previous backup, DELTA backup do not rely on it */
 #define FILE_NOT_FOUND		(-2) /* file disappeared during backup */
 #define BLOCKNUM_INVALID	(-1)
-#define PROGRAM_VERSION	"2.1.5"
-#define AGENT_PROTOCOL_VERSION 20105
+#define PROGRAM_VERSION	"2.2.0"
+#define AGENT_PROTOCOL_VERSION 20200
 
 
 typedef struct ConnectionOptions
