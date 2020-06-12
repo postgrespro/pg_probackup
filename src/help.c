41d4013a81c616c8fd19a4aaa98cdc0224897876
Grigory Smolkin <g.smolkin@postgrespro.ru>
2020-06-12 18:57:47 +0300

    [Issue #66] fix help

--- src/help.c
+++ src/help.c
@@ -160,7 +160,7 @@ help_pg_probackup(void)
 	printf(_("                 [-T OLDDIR=NEWDIR] [--progress]\n"));
 	printf(_("                 [--external-mapping=OLDDIR=NEWDIR]\n"));
 	printf(_("                 [--skip-external-dirs] [--restore-command=cmdline]\n"));
-	printf(_("                 [--incremental-mode=none|checksum|lsn\n"));
+	printf(_("                 [--incremental-mode=none|checksum|lsn]\n"));
 	printf(_("                 [--no-sync]\n"));
 	printf(_("                 [--db-include | --db-exclude]\n"));
 	printf(_("                 [--remote-proto] [--remote-host]\n"));
@@ -398,7 +398,7 @@ help_restore(void)
 	printf(_("                 [-T OLDDIR=NEWDIR]\n"));
 	printf(_("                 [--external-mapping=OLDDIR=NEWDIR]\n"));
 	printf(_("                 [--skip-external-dirs]\n"));
-	printf(_("                 [--incremental-mode=none|checksum|lsn\n"));
+	printf(_("                 [--incremental-mode=none|checksum|lsn]\n"));
 	printf(_("                 [--db-include dbname | --db-exclude dbname]\n"));
 	printf(_("                 [--recovery-target-time=time|--recovery-target-xid=xid\n"));
 	printf(_("                  |--recovery-target-lsn=lsn [--recovery-target-inclusive=boolean]]\n"));
