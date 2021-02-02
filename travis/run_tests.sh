ab9cb21dc9a79e591334f8b67c92c78371020680
anastasia <a.lubennikova@postgrespro.ru>
2021-02-02 20:31:08 +0300

    fix gcov flags

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -55,7 +55,8 @@ pg_config
 
 # Build and install pg_probackup (using PG_CPPFLAGS and SHLIB_LINK for gcov)
 echo "############### Compiling and installing pg_probackup:"
-make USE_PGXS=1 PG_CPPFLAGS="-coverage" SHLIB_LINK="-coverage" top_srcdir=$PG_SRC install
+make USE_PGXS=1 PG_CPPFLAGS="-coverage" SHLIB_LINK="-coverage" top_srcdir=$PG_SRC
+make USE_PGXS=1 top_srcdir=$PG_SRC install
 
 # Setup python environment
 echo "############### Setting up python env:"
