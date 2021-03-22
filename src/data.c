59dc864483917ec4bd620bb63d3c30488ae8b990
Grigory Smolkin <g.smolkin@postgrespro.ru>
2021-03-22 21:32:52 +0300

    open header file in append mode

--- src/data.c
+++ src/data.c
@@ -2268,7 +2268,7 @@ write_page_headers(BackupPageHeader2 *headers, pgFile *file, HeaderMap *hdr_map,
 	{
 		elog(LOG, "Creating page header map \"%s\"", map_path);
 
-		hdr_map->fp = fopen(map_path, PG_BINARY_W);
+		hdr_map->fp = fopen(map_path, 'a');
 		if (hdr_map->fp == NULL)
 			elog(ERROR, "Cannot open header file \"%s\": %s",
 				 map_path, strerror(errno));
