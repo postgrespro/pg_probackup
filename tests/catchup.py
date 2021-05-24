eab58c18f6dd62424d5c9e7cc84041900adf51f8
Mikhail A. Kulagin <m.kulagin@postgrespro.ru>
2021-05-24 19:29:32 +0300

    simple delta catchup test

--- tests/catchup.py
+++ tests/catchup.py
@@ -89,7 +89,7 @@ class CatchupTest(ProbackupTest, unittest.TestCase):
         self.del_test_dir(module_name, fname)
 
     # @unittest.skip("skip")
-    def test_remote_catchup(self):
+    def test_remote_ptrack_catchup(self):
         """
         Test 'catchup' mode
         create node,
@@ -150,4 +150,64 @@ class CatchupTest(ProbackupTest, unittest.TestCase):
         # Clean after yourself
         self.del_test_dir(module_name, fname)
 
+    # @unittest.skip("skip")
+    def test_remote_delta_catchup(self):
+        """
+        Test 'catchup' mode
+        create node,
+        make a copy with replication, start copy, stop copy,
+        generate some load on master, insert some test data on master,
+        catchup copy, start and select test data
+        """
+        fname = self.id().split('.')[3]
 
+        # prepare master
+        source_pg = self.make_simple_node(
+            base_dir = os.path.join(module_name, fname, 'src'),
+            set_replication = True,
+            ptrack_enable = True,
+            initdb_params = ['--data-checksums']
+            )
+        source_pg.slow_start()
+        source_pg.safe_psql("postgres", "CREATE EXTENSION ptrack")
+        source_pg.safe_psql("postgres", "CREATE TABLE ultimate_question(answer int)")
+
+        # make clean shutdowned lagging behind replica
+        dest_pg = self.make_empty_node(os.path.join(module_name, fname, 'dst'))
+        dest_pg = self.catchup_node(
+            backup_mode = 'FULL',
+            source_pgdata = source_pg.data_dir,
+            destination_node = dest_pg,
+            options = ['-d', 'postgres', '-p', str(source_pg.port), '--stream'])
+        self.set_replica(source_pg, dest_pg)
+        dest_pg.slow_start(replica = True)
+        dest_pg.stop()
+
+        # make changes on master
+        source_pg.pgbench_init(scale=10)
+        pgbench = source_pg.pgbench(options=['-T', '10', '--no-vacuum'])
+        pgbench.wait()
+        source_pg.safe_psql("postgres", "INSERT INTO ultimate_question VALUES(42)")
+        result = source_pg.safe_psql("postgres", "SELECT * FROM ultimate_question")
+
+        # catchup
+        self.catchup_node(
+            backup_mode = 'DELTA',
+            source_pgdata = source_pg.data_dir,
+            destination_node = dest_pg,
+            options = ['-d', 'postgres', '-p', str(source_pg.port), '--stream'])
+
+        # stop replication
+        source_pg.stop()
+
+        # check latest changes
+        self.set_replica(source_pg, dest_pg)
+        dest_pg.slow_start(replica = True)
+        self.assertEqual(
+            result,
+            dest_pg.safe_psql("postgres", "SELECT * FROM ultimate_question"),
+            'Different answer from copy')
+        dest_pg.stop()
+
+        # Clean after yourself
+        self.del_test_dir(module_name, fname)
