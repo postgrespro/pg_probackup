a748784b086adbddd743936ae3a730b7dde40867
Grigory Smolkin <g.smolkin@postgrespro.ru>
2021-08-07 13:32:41 +0300

    [skip travis] pkg testing infrastructure

---
+++ packaging/test/scripts/pg_probackup_forks_deb.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+# Copyright Notice:
+# © (C) Postgres Professional 2015-2016 http://www.postgrespro.ru/
+# Distributed under Apache License 2.0
+# Распространяется по лицензии Apache 2.0
+
+set -xe
+set -o pipefail
+
+# fix https://github.com/moby/moby/issues/23137
+ulimit -n 1024
+
+if [ ${EDITION} == 'ent' ]; then
+    exit 0
+fi
+
+# upgrade and utils
+# export parameters
+export DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
+#if [ ${CODENAME} == 'jessie' ]; then
+#printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
+#fi
+
+apt-get -qq update
+apt-get -qq install -y wget nginx gnupg lsb-release apt-transport-https
+#apt-get -qq install -y libterm-readline-gnu-perl dialog gnupg procps
+
+# echo -e 'Package:  *\nPin: origin test.postgrespro.ru\nPin-Priority: 800' >\
+#  /etc/apt/preferences.d/pgpro-800
+
+# install nginx
+echo "127.0.0.1 test.postgrespro.ru" >> /etc/hosts
+cat <<EOF > /etc/nginx/nginx.conf
+user www-data;
+worker_processes  1;
+error_log  /var/log/nginx/error.log;
+events {
+    worker_connections  1024;
+}
+http {
+    server {
+        listen   80 default;
+        root /var/www;
+    }
+}
+EOF
+nginx -s reload || (pkill -9 nginx || nginx -c /etc/nginx/nginx.conf &)
+
+# add postgresql repo
+if [ ${PG_VERSION} == '9.6' ]; then
+  sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  apt-get update -y
+fi
+
+# install POSTGRESPRO
+if [ ${EDITION} == 'std' ]; then
+  sh -c 'echo "deb https://repo.postgrespro.ru/pgpro-${PG_VERSION}/${DISTRIB}/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/pgpro.list'
+  wget --quiet -O - https://repo.postgrespro.ru/pgpro-${PG_VERSION}/keys/GPG-KEY-POSTGRESPRO | apt-key add -
+  apt-get update -y
+#  apt-get install -y postgrespro-std-${PG_VERSION}
+
+  if [[ ${PG_VERSION} == '9.6' ]]; then
+      apt-get install -y postgrespro-${PG_VERSION}
+      BINDIR="/usr/lib/postgresql/${PG_VERSION}/bin"
+  else
+      apt-get install -y postgrespro-std-${PG_VERSION}
+      BINDIR="/opt/pgpro/std-${PG_VERSION}/bin"
+      export LD_LIBRARY_PATH=/opt/pgpro/std-${PG_VERSION}/lib/
+  fi
+fi
+
+# install pg_probackup from current public repo
+echo "deb [arch=amd64] http://repo.postgrespro.ru/pg_probackup-forks/deb/ $(lsb_release -cs) main-$(lsb_release -cs)" >\
+  /etc/apt/sources.list.d/pg_probackup-old.list
+wget -O - http://repo.postgrespro.ru/pg_probackup-forks/keys/GPG-KEY-PG_PROBACKUP | apt-key add - && apt-get update
+
+apt-get install -y pg-probackup-${EDITION}-${PG_VERSION}
+pg_probackup-${EDITION}-${PG_VERSION} --help
+pg_probackup-${EDITION}-${PG_VERSION} --version
+
+
+if [ ${EDITION} == 'std' ]; then
+  export PGDATA=/tmp/data
+  su postgres -c "${BINDIR}/initdb -k -D ${PGDATA}"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} init -B /tmp/backup"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} add-instance --instance=node -B /tmp/backup -D ${PGDATA}"
+
+  echo "wal_level=hot_standby" >> ${PGDATA}/postgresql.auto.conf
+  echo "fsync=off" >> ${PGDATA}/postgresql.auto.conf
+  echo "archive_mode=on" >> ${PGDATA}/postgresql.auto.conf
+  echo "archive_command='pg_probackup-${EDITION}-${PG_VERSION} archive-push --no-sync -B /tmp/backup compress --instance=node --wal-file-path %p --wal-file-name %f'" >> ${PGDATA}/postgresql.auto.conf
+
+  su postgres -c "${BINDIR}/pg_ctl stop -w -t 60 -D /var/lib/pgpro/std-${PG_VERSION}/data" || echo "it is all good"
+  su postgres -c "${BINDIR}/pg_ctl start -D ${PGDATA}"
+  sleep 5
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} backup --instance=node -b full -B /tmp/backup -D ${PGDATA} --no-sync"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} show --instance=node -B /tmp/backup -D ${PGDATA}"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} show --instance=node -B /tmp/backup -D ${PGDATA} --archive"
+
+  su postgres -c "${BINDIR}/pgbench --no-vacuum -i -s 5"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} backup --instance=node -b page -B /tmp/backup -D ${PGDATA} --no-sync"
+fi
+
+# install new packages
+echo "deb [arch=amd64] http://test.postgrespro.ru/pg_probackup-forks/deb/ $(lsb_release -cs) main-$(lsb_release -cs)" >\
+	/etc/apt/sources.list.d/pg_probackup-new.list
+wget -O - http://test.postgrespro.ru/pg_probackup-forks/keys/GPG-KEY-PG_PROBACKUP | apt-key add -
+apt-get update -y
+
+#if [ ${EDITION} == 'std' ] && [ ${PG_VERSION} == '9.6' ]; then
+#  apt-get install -y libpq5 pg-probackup-${EDITION}-${PG_VERSION}
+#else
+#  apt-get install -y pg-probackup-${EDITION}-${PG_VERSION}
+#fi
+
+apt-get install -y pg-probackup-${EDITION}-${PG_VERSION}
+
+# in Ent 11 and 10 because of PQselect vanilla libpq5 is incompatible with Ent pg_probackup
+if [ ${EDITION} == 'ent' ]; then
+  if [ ${PG_VERSION} == '11' ] || [ ${PG_VERSION} == '10' ] || [ ${PG_VERSION} == '9.6' ]; then
+    exit 0
+  fi
+fi
+
+pg_probackup-${EDITION}-${PG_VERSION} --help
+pg_probackup-${EDITION}-${PG_VERSION} --version
+
+if [ ${EDITION} == 'ent' ]; then
+  exit 0
+fi
+
+if [ ${EDITION} == 'std' ] && [ ${PG_VERSION} == '9.6' ]; then
+  exit 0
+fi
+
+
+#if [ ${CODENAME} == 'precise' ] && [ ${PG_VERSION} != '10' ] && [ ${PG_VERSION} != '11' ]; then
+  su postgres -c "${BINDIR}/pgbench --no-vacuum -t 1000 -c 1"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} backup --instance=node -b page -B /tmp/backup -D ${PGDATA}"
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} show --instance=node -B /tmp/backup -D ${PGDATA}"
+
+  su postgres -c "${BINDIR}/pg_ctl stop -w -t 60 -D ${PGDATA}"
+  rm -rf ${PGDATA}
+
+  su postgres -c "pg_probackup-${EDITION}-${PG_VERSION} restore --instance=node -B /tmp/backup -D ${PGDATA}"
+  su postgres -c "${BINDIR}/pg_ctl start -w -t 60 -D ${PGDATA}"
+
+sleep 5
+echo "select count(*) from pgbench_accounts;" | su postgres -c "${BINDIR}/psql" || exit 1
+
+exit 0
---
+++ packaging/test/scripts/rpm.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+# Copyright Notice:
+# © (C) Postgres Professional 2015-2016 http://www.postgrespro.ru/
+# Distributed under Apache License 2.0
+# Распространяется по лицензии Apache 2.0
+
+set -xe
+set -o pipefail
+
+# fix https://github.com/moby/moby/issues/23137
+ulimit -n 1024
+
+PG_TOG=$(echo $PG_VERSION | sed 's|\.||g')
+
+# yum upgrade -y || echo 'some packages in docker failed to upgrade'
+# yum install -y sudo
+if [ ${DISTRIB} == 'rhel' ] && [ ${PG_TOG} == '13' ]; then # no packages for PG13 on PGDG
+    exit 0
+fi
+
+if [ ${DISTRIB} == 'oraclelinux' ] &&  [ ${DISTRIB_VERSION} == '6' ] && [ ${PG_TOG} == '13' ]; then # no packages for PG13 on PGDG
+    exit 0
+fi
+
+if [ ${DISTRIB_VERSION} == '6' ]; then
+    yum install -y https://nginx.org/packages/rhel/6/x86_64/RPMS/nginx-1.8.1-1.el6.ngx.x86_64.rpm
+elif [ ${DISTRIB} == 'oraclelinux' ] && [ ${DISTRIB_VERSION} == '8' ]; then
+    yum install -y nginx
+elif [ ${DISTRIB_VERSION} == '7' ]; then
+    yum install -y https://nginx.org/packages/rhel/7/x86_64/RPMS/nginx-1.8.1-1.el7.ngx.x86_64.rpm
+else
+    yum install epel-release -y
+    yum install -y nginx
+fi
+
+if ! getent group nginx > /dev/null 2>&1 ; then
+    addgroup --system --quiet nginx
+fi
+if ! getent passwd nginx > /dev/null 2>&1 ; then
+    adduser --quiet \
+        --system --disabled-login --ingroup nginx \
+        --home /var/run/nginx/ --no-create-home \
+        nginx
+fi
+
+cat <<EOF > /etc/nginx/nginx.conf
+user nginx;
+worker_processes  1;
+error_log  /var/log/nginx/error.log;
+events {
+    worker_connections  1024;
+}
+http {
+    server {
+        listen   80 default;
+        root /var/www;
+    }
+}
+EOF
+nginx -s reload || (pkill -9 nginx || nginx -c /etc/nginx/nginx.conf &)
+
+# install POSTGRESQL
+rpm -ivh https://download.postgresql.org/pub/repos/yum/reporpms/EL-${DISTRIB_VERSION}-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+if [ ${DISTRIB} == 'oraclelinux' ] && [ ${DISTRIB_VERSION} == '8' ]; then
+    dnf -qy module disable postgresql
+fi
+
+if [ ${DISTRIB} == 'centos' ] && [ ${DISTRIB_VERSION} == '8' ]; then
+    dnf -qy module disable postgresql
+fi
+
+yum install -y postgresql${PG_TOG}-server.x86_64
+export PGDATA=/var/lib/pgsql/${PG_VERSION}/data
+
+# install old packages
+yum install -y http://repo.postgrespro.ru/pg_probackup/keys/pg_probackup-repo-${DISTRIB}.noarch.rpm
+yum install -y ${PKG_NAME}
+${PKG_NAME} --help
+${PKG_NAME} --version
+
+su postgres -c "/usr/pgsql-${PG_VERSION}/bin/initdb -k -D ${PGDATA}"
+echo "fsync=off" >> ${PGDATA}/postgresql.auto.conf
+echo "wal_level=hot_standby" >> ${PGDATA}/postgresql.auto.conf
+echo "archive_mode=on" >> ${PGDATA}/postgresql.auto.conf
+echo "archive_command='${PKG_NAME} archive-push --no-sync -B /tmp/backup --instance=node --wal-file-path %p --wal-file-name %f'" >> ${PGDATA}/postgresql.auto.conf
+su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pg_ctl start -D ${PGDATA}"
+sleep 5
+
+su postgres -c "${PKG_NAME} init -B /tmp/backup"
+su postgres -c "${PKG_NAME} add-instance --instance=node -B /tmp/backup -D ${PGDATA}"
+su postgres -c "${PKG_NAME} backup --instance=node --compress -b full -B /tmp/backup -D ${PGDATA} --no-sync"
+su postgres -c "${PKG_NAME} show --instance=node -B /tmp/backup -D ${PGDATA} --archive"
+
+su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pgbench --no-vacuum -i -s 5"
+su postgres -c "${PKG_NAME} backup --instance=node -b page -B /tmp/backup -D ${PGDATA} --no-sync"
+
+# install new packages
+echo "127.0.0.1 repo.postgrespro.ru" >> /etc/hosts
+
+# yum remove -y pg_probackup-repo
+#yum install -y http://repo.postgrespro.ru/pg_probackup/keys/pg_probackup-repo-${DISTRIB}.noarch.rpm
+yum clean all -y
+
+sed -i "s/https/http/g" /etc/yum.repos.d/pg_probackup.repo
+
+yum update -y ${PKG_NAME}
+${PKG_NAME} --help
+${PKG_NAME} --version
+
+su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pgbench --no-vacuum -t 1000 -c 1"
+su postgres -c "${PKG_NAME} backup --instance=node -b page -B /tmp/backup -D ${PGDATA} --no-sync"
+su postgres -c "${PKG_NAME} show --instance=node -B /tmp/backup -D ${PGDATA}"
+
+su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pg_ctl stop -D ${PGDATA}"
+rm -rf ${PGDATA}
+
+su postgres -c "${PKG_NAME} restore --instance=node -B /tmp/backup -D ${PGDATA} --no-sync"
+su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pg_ctl start -w -D ${PGDATA}"
+
+sleep 5
+
+echo "select count(*) from pgbench_accounts;" | su postgres -c "/usr/pgsql-${PG_VERSION}/bin/psql" || exit 1
+
+#else
+#    echo "127.0.0.1 repo.postgrespro.ru" >> /etc/hosts
+#    rpm -ivh http://repo.postgrespro.ru/pg_probackup/keys/pg_probackup-repo-${DISTRIB}.noarch.rpm
+#    yum install -y ${PKG_NAME}
+#    ${PKG_NAME} --help
+#    su postgres -c "/usr/pgsql-${PG_VERSION}/bin/initdb -k -D ${PGDATA}"
+#    su postgres -c "${PKG_NAME} init -B /tmp/backup"
+#    su postgres -c "${PKG_NAME} add-instance --instance=node -B /tmp/backup -D ${PGDATA}"
+#    echo "wal_level=hot_standby" >> ${PGDATA}/postgresql.auto.conf
+#    echo "archive_mode=on" >> ${PGDATA}/postgresql.auto.conf
+#    echo "archive_command='${PKG_NAME} archive-push -B /tmp/backup --instance=node --wal-file-path %p --wal-file-name %f'" >> ${PGDATA}/postgresql.auto.conf
+#    su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pg_ctl start -D ${PGDATA}"
+#    sleep 5
+#    su postgres -c "${PKG_NAME} backup --instance=node --compress -b full -B /tmp/backup -D ${PGDATA}"
+#    su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pgbench --no-vacuum -i -s 10"
+#    su postgres -c "${PKG_NAME} backup --instance=node -b page -B /tmp/backup -D ${PGDATA}"
+#    su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pgbench --no-vacuum -t 1000 -c 1"
+#    su postgres -c "${PKG_NAME} backup --instance=node -b page -B /tmp/backup -D ${PGDATA}"
+#    su postgres -c "${PKG_NAME} show --instance=node -B /tmp/backup -D ${PGDATA}"
+#    su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pg_ctl stop -D ${PGDATA}"
+#    rm -rf ${PGDATA}
+#    su postgres -c "${PKG_NAME} restore --instance=node -B /tmp/backup -D ${PGDATA}"
+#    su postgres -c "/usr/pgsql-${PG_VERSION}/bin/pg_ctl start -D ${PGDATA}"
+#    sleep 10
+#    echo "select count(*) from pgbench_accounts;" | su postgres -c "/usr/pgsql-${PG_VERSION}/bin/psql" || exit 1
+#fi
+
+exit 0 # while PG12 is not working
+
+# SRC PACKAGE
+cd /mnt
+yum install yum-utils rpm-build -y
+yumdownloader --source ${PKG_NAME}
+rpm -ivh ./*.rpm
+cd /root/rpmbuild/SPECS
+exit 0
+
+# build pg_probackup
+yum-builddep -y pg_probackup.spec
+rpmbuild -bs pg_probackup.spec
+rpmbuild -ba pg_probackup.spec #2>&1 | tee -ai /app/out/build.log
