274309b1c6df1e15faad43144f1321767b486b64
PavelGilev <p.gilev@postgrespro.ru>
2021-09-27 12:12:08 +0700

    13 th Try run compatibility

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -86,18 +86,17 @@ pg_config
 echo "############### kernel params:"
 cat /proc/sys/kernel/yama/ptrace_scope
 sudo sysctl kernel.yama.ptrace_scope=0
-cat /proc/sys/kernel/yama/ptrace_scope
-if [ not "$OLD_BIN_ON" = on]
-    echo "############### Compiling and installing pg_probackup old:"
-    mkdir pg_old
-    cd pg_old
-    git clone https://github.com/postgrespro/pg_probackup.git
-    cd pg_probackup
-    git checkout 2.4.10
-    make USE_PGXS=1 top_srcdir=$PG_SRC install
-    cd ../..
-    export PGPROBACKUPBIN_OLD=/pg/testdir/pg_old/pg_probackup/pg_probackup
-fi
+
+echo "############### Compiling and installing pg_probackup old:"
+mkdir pg_old
+cd pg_old
+git clone https://github.com/postgrespro/pg_probackup.git
+cd pg_probackup
+git checkout 2.4.10
+make USE_PGXS=1 top_srcdir=$PG_SRC install
+cd ../..
+export PGPROBACKUPBIN_OLD=/pg/testdir/pg_old/pg_probackup/pg_probackup
+
 
 # Build and install pg_probackup (using PG_CPPFLAGS and SHLIB_LINK for gcov)
 echo "############### Compiling and installing pg_probackup:"
