94caeb1793feddc0fe13b5f94615efcd0ca776dd
d.lepikhova <d.lepikhova@postgrespro.ru>
2022-06-29 22:19:31 +0500

    Shorthand return-expression

--- tests/helpers/ptrack_helpers.py
+++ tests/helpers/ptrack_helpers.py
@@ -100,11 +100,7 @@ def is_enterprise():
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
-    if b'enterprise' in p.communicate()[0]:
-        return True
-    else:
-        return False
-
+    return b'postgrespro.ru' in p.communicate()[0]
 
 class ProbackupException(Exception):
     def __init__(self, message, cmd):
