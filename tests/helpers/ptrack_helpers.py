19a7c5b01f678cc2ef458e4fd3cd017378de79bf
Yura Sokolov <funny.falcon@gmail.com>
2022-11-25 07:59:14 +0300

    tests: do not read whole file at once for digest. Iterate instead.

--- tests/helpers/ptrack_helpers.py
+++ tests/helpers/ptrack_helpers.py
@@ -1774,9 +1774,9 @@ class ProbackupTest(object):
                 file_relpath = os.path.relpath(file_fullpath, pgdata)
                 directory_dict['files'][file_relpath] = {'is_datafile': False}
                 with open(file_fullpath, 'rb') as f:
-                    content = f.read()
                     # truncate cfm's content's zero tail
                     if file_relpath.endswith('.cfm'):
+                        content = f.read()
                         zero64 = b"\x00"*64
                         l = len(content)
                         while l > 64:
@@ -1785,9 +1785,14 @@ class ProbackupTest(object):
                                 break
                             l = s
                         content = content[:l]
-                    directory_dict['files'][file_relpath]['md5'] = hashlib.md5(content).hexdigest()
-#                directory_dict['files'][file_relpath]['md5'] = hashlib.md5(
-#                    f = open(file_fullpath, 'rb').read()).hexdigest()
+                        digest = hashlib.md5(content)
+                    else:
+                        digest = hashlib.md5()
+                        while True:
+                            b = f.read(64*1024)
+                            if not b: break
+                            digest.update(b)
+                    directory_dict['files'][file_relpath]['md5'] = digest.hexdigest()
 
                 # crappy algorithm
                 if file.isdigit():
